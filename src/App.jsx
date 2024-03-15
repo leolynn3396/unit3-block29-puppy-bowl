d1423fa8fd702d17dcafeb96185a4937f8a42fdb
Ling Huang <linghuang@Lings-MacBook-Pro.local>
2024-03-14 18:27:41 -0700

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+/*
+Player List - 
+User can see a list of each player competing in the bowl
+
+Details- 
+The user can click a details button (or something similar) on each puppy that will lead them to another page view with specific details on that puppy, such as owner and team name.
+
+Search Bar- 
+Users can search for a specific player in a search bar and see a new list of players with names matching the text in the search bar.
+
+Create Player Form - 
+Somewhere in the application, there is a form to create a new player and it gets added to the all players list without refreshing the page
+*/
+import { BrowserRouter } from "react-router-dom";
 import { useState, useEffect } from 'react'
 import { getPlayers } from './api';
 import Player from './components/Player';
--- src/api.js
+++ src/api.js
@@ -1,28 +1,41 @@
-const API_URL = 'https://fsa-puppy-bowl.herokuapp.com/api/2308-ACC-ET-WEB-PT/players';
+const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/2308-ACC-ET-WEB-PT/players`;
 
 export async function getPlayers() {
     try {
-        const response = await fetch('${API_URL}/players');
+        const response = await fetch(`${API_URL}/players`);
         const result = await response.json();
         return result.data.players;
     } catch (error) {
         console.error(error)
     }
-
 }
 
 export async function getPlayer(playerId) {
     try {
-        const response =await fetch('${API_URL}/players/${playerId}');
+        const response = await fetch(`${API_URL}/players/${playerId}`);
         const result = await response.json();
         return result.data.players;
-    }catch (error) {
+    } catch (error) {
         console.error(error);
     }
-    }
+}
 
-export async function createPlayer(player) {
+export async function createPlayer(name, breed) {
     try {
-
+        const response = await fetch(`${API_URL}/players`,{
+        method: "POST",
+            headers: {
+            "Contect-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            breed
+        })
+    });
+    const result = await response.json();
+    return result;
+} catch (error) {
+    console.error(error);
+}
     }
 
